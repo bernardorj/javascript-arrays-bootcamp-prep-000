b06292554066de25d1175e784bccb7b14e0a2ee3
Rachel Bernardo <racheljbernardo@gmail.com>
2017-10-12 19:48:39 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -13,7 +13,7 @@ function addElementToBeginningOfArray(array, element){
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element){
-  var newArray = array.unshift(element)
+  var array = array.unshift(element)
 
   return newArray
 }
