5e81e1ea93b6b7dcbd70b82ac0191cd15722cd5d
Rachel Bernardo <racheljbernardo@gmail.com>
2017-10-12 20:11:31 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -33,3 +33,7 @@ function destructivelyAddElementToEndOfArray(array, element){
 function accessElementInArray(array, index) {
   return array[index];
 }
+
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  array.shift();
+}
