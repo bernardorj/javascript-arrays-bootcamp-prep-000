bb6874075ceebf3bf3657fc21bc39b117a91e4d3
Rachel Bernardo <racheljbernardo@gmail.com>
2017-10-12 18:58:22 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -6,24 +6,36 @@ var chocolateBars = [
 ]
 
 function addElementToBeginningOfArray(array, element){
+  var array = chocolateBars;
+  var element = 'foo'
+
   var newArray = [element, ...array]
 
   return newArray
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element){
+  var array = chocolateBars;
+  var element = 'foo'
+
   var newArray2 = array.unshift(element)
 
   return newArray2
 }
 
 function addElementToEndOfArray(array, element){
+  var array = chocolateBars;
+  var element = 'foo'
+
   var newArray3 = [...array, element]
 
   return newArray3
 }
 
 function destructivelyAddElementToEndOfArray(array, element){
+  var array = chocolateBars;
+  var element = 'foo'
+  
   var newArray4 = chocolateBars.push(element)
 
   return newArray4
